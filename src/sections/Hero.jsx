29cc63e1dcbdc67741ee3949a99f65324d1823b9
Mohit Kumar <mohitkd4444@gmail.com>
2023-08-28 23:02:07 +0530

    Second Commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,31 +1,35 @@
 import Button from "../components/Button"
 import { arrowRight } from '../assets/icons'
 import { statistics } from '../constants'
+import {bigShoe1} from '../assets/images'
 
 const Hero = () => {
   return (
     <section
       id="hero"
-      className="w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      className="flex flex-col justify-center w-full min-h-screen gap-10 p-2 border-2 border-red-500 xl:flex-row max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
-        <p>Our Summer Collections</p>
-        <h1>
-          <span>The New Arrival</span>
+      <div className="relative flex flex-col items-start justify-center w-full xl:w-2/5 max-xl:padding-x pt-28 ">
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="relative z-10 pr-10 xl:bg-white xl:whitespace-nowrap">The New Arrival</span>
           <br />
-          <span>Nike</span>
-          Shoes
+          <span className="inline-block mt-3 text-coral-red">Nike</span> Shoes
         </h1>
-        <p>Discover stylish Nike arrivals,quality comfort, and innovation for your active life</p>
+        <p className="mt-6 text-lg leading-8 font-montserrat text-slate-gray mb-14 sm:max-w-sm">Discover stylish Nike arrivals,quality comfort, and innovation for your active life</p>
         <Button label="Shop now" iconURL={arrowRight} />
-        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16 ">
+        <div className="flex flex-wrap items-start justify-start w-full gap-16 mt-20">
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-bold font-palanquin">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="relative flex items-center justify-center flex-1 bg-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-color">
+        <img src={bigShoe1} alt="shoes collection" width={610} height={500} className="relative z-10 object-contain" />
       </div>
     </section>
   )
